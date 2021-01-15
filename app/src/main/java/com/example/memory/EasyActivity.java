6326bcfcbd316e6de958ce47c6f556e2cb96bfb4
Joey Mafukidze <mafukidzejoey@gmail.com>
2021-01-15 23:43:48 +0100

    Added Easy Layout

---
+++ app/src/main/java/com/example/memory/EasyActivity.java
@@ -0,0 +1,14 @@
+package com.example.memory;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EasyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy);
+    }
+}
--- app/src/main/java/com/example/memory/MainActivity.java
+++ app/src/main/java/com/example/memory/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.memory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button easy;
+    Button medium;
+    Button hard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         easy = findViewById(R.id.button2);
+         medium = findViewById(R.id.button3);
+         hard = findViewById(R.id.button4);
+
+         easy.setOnClickListener(v -> {
+             Intent startEasyActivity = new Intent(MainActivity.this, EasyActivity.class);
+             Toast t = Toast.makeText(this, "Loading", Toast.LENGTH_LONG);
+             t.show();
+             startActivity(startEasyActivity);
+         });
+
+        medium.setOnClickListener(v -> {
+            Intent startMediumActivity = new Intent(MainActivity.this, MediumActivity.class);
+            Toast t = Toast.makeText(this, "Loading", Toast.LENGTH_LONG);
+            t.show();
+            startActivity(startMediumActivity);
+        });
+
+        hard.setOnClickListener(v -> {
+            Intent startHardActivity = new Intent(MainActivity.this, HardActivity.class);
+            Toast t = Toast.makeText(this, "Loading", Toast.LENGTH_LONG);
+            t.show();
+            startActivity(startHardActivity);
+        });
     }
 }
